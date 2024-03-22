1b9dcd62c38fe7d418aa483b99e010d007e3ada6
xanderlifeftoahacked <freztshow@gmail.com>
2024-03-22 21:21:12 +0300

    added new buttons

--- src/commands/travel_helper.py
+++ src/commands/travel_helper.py
@@ -5,3 +5,9 @@ class Commands(Enum):
     MAKING_ROUTES = 'Построение маршрутов'
     MAKE_ROUTE_TO_START = 'Маршрут до первой точки'
     MAKE_ROUTE_OF_TRAVEL = 'Маршрут по точкам путешествия'
+
+    WEATHER_INFO = 'Информация о погоде'
+    HOTEL_INFO = 'Отели'
+    ATTRACTIONS_INFO = 'Достопримечательности'
+    CAFES_INFO = 'Кафе и рестораны'
+    SECRET_INFO = 'Секретная кнопка'
