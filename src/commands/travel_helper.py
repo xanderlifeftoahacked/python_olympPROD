5d60f1ab1f8fe431dcabb5582ee7f02ba062727a
xanderlifeftoahacked <freztshow@gmail.com>
2024-03-23 13:26:13 +0300

    updated keyboard

--- src/commands/travel_helper.py
+++ src/commands/travel_helper.py
@@ -6,8 +6,8 @@ class Commands(Enum):
     MAKE_ROUTE_TO_START = 'Маршрут до первой точки'
     MAKE_ROUTE_OF_TRAVEL = 'Маршрут по точкам путешествия'
 
-    WEATHER_INFO = 'Информация о погоде'
+    WEATHER_INFO = 'Погода'
     HOTEL_INFO = 'Отели'
     ATTRACTIONS_INFO = 'Достопримечательности'
     CAFES_INFO = 'Кафе и рестораны'
-    SECRET_INFO = 'Секретная кнопка'
+    FRIEND_FIND = 'Найти друга'
