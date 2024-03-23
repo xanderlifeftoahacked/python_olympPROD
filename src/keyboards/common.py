c6b58b48fb95c18dcea9e7a2a0dfa748dba3646e
xanderlifeftoahacked <freztshow@gmail.com>
2024-03-23 13:40:33 +0300

    updated keyboard layout

--- src/keyboards/common.py
+++ src/keyboards/common.py
@@ -10,7 +10,7 @@ kb_input = types.InlineKeyboardMarkup(inline_keyboard=[[inline_button(
     CommonCommands.END_INPUT.value)]])
 
 kb_is_valid = types.InlineKeyboardMarkup(inline_keyboard=[[inline_button(
-    CommonCommands.GOOD.value)], [inline_button(CommonCommands.BAD.value)]])
+    CommonCommands.GOOD.value), inline_button(CommonCommands.BAD.value)]])
 
 kb_main = types.ReplyKeyboardMarkup(resize_keyboard=True, keyboard=[[button(CommonCommands.INFO.value)],
                                                                     [button(CommonCommands.MY_PROFILE.value)],  [button(CommonCommands.MENU_TRAVELS.value)]])
