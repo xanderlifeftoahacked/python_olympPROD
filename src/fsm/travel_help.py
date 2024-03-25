7de6f8fcafabe23fc8bf30a82c025b249fe7e9f7
xanderlifeftoahacked <freztshow@gmail.com>
2024-03-25 18:56:32 +0000

    added cafes

--- src/fsm/travel_help.py
+++ src/fsm/travel_help.py
@@ -4,5 +4,5 @@ from aiogram.fsm.state import StatesGroup, State
 
 class Helper(StatesGroup):
     choosing_place = State()
+    choosing_cafe = State()
     choosing_hotel = State()
-    choosing_place = State()
